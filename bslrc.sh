7d94350feb106367e07ebf552a88945deffc4b3a
Adam Bullmer <psycodrumfreak@gmail.com>
2014-11-07 04:38:03 -0600

    Fixed bug to allow the bash prompt to be set by another bash commant

--- bslrc.sh
+++ bslrc.sh
@@ -6,13 +6,13 @@ source $BSL/bsl_setter.sh
 
 if test $PROMPT_COMMAND
     then
-        WRAPPED_PROMPT_COMMAND= $PROMPT_COMMAND
+        WRAPPED_PROMPT_COMMAND=${PROMPT_COMMAND}
 fi
 
 bsl_update_prompt() {
     if test $WRAPPED_PROMPT_COMMAND
         then
-            WRAPPED_PROMPT_COMMAND
+            ${WRAPPED_PROMPT_COMMAND}
     fi
 
     update_status_line
